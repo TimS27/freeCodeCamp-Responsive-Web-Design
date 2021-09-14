aa80ddb1a5fc7ec43400b949eef1ad0241d0b595
Tim <82032094+TimS27@users.noreply.github.com>
2021-09-14 15:43:52 +0200

    Re-Upload of technicalDocumentation Page

--- technicalDocumentationPage/script.js
+++ technicalDocumentationPage/script.js
@@ -1,8 +1,12 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function openNav() {
+  document.getElementById("navbar").style.display = "flex";
+  document.getElementById("main-doc").style.paddingLeft = "245px";
+  document.getElementById("openbtn").style.display = "flex";
+  document.getElementById("openbtn").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("navbar").style.display = "none";
+  document.getElementById("main-doc").style.paddingLeft = "10px";
+  document.getElementById("openbtn").style.display = "flex";
 }
